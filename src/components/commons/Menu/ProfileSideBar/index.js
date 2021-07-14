62ff66ad052d527202a243c3edc0079bf4caf0ab
Felipe Vash <felipe.silva@atonit.com.br>
2021-07-14 14:11:12 -0300

    fix: fix da  fix

---
+++ src/components/commons/Menu/ProfileSideBar/index.js
@@ -0,0 +1,20 @@
+import AlurakutProfileSidebarMenuDefault from '../Default';
+
+export default function AlurakutMenuProfileSidebar({ githubUser, name }) {
+  return (
+    <div className="alurakutMenuProfileSidebar">
+      <div>
+        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
+        <hr />
+        <p>
+          <a className="boxLink" href={`/user/${githubUser}`}>
+            {name}
+          </a>
+        </p>
+        <hr />
+
+        <AlurakutProfileSidebarMenuDefault />
+      </div>
+    </div>
+  )
+}
